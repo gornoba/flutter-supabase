a95277321e28312ad464ab2da53756a050e950f3
HyunHoo Im <gornoba@naver.com>
2024-08-13 10:10:01 +0900

    chore: Add bottom navigation bar to MainScreen

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:food_pick_app/screen/favorite_screen.dart';
+import 'package:food_pick_app/screen/home_screen.dart';
+import 'package:food_pick_app/screen/my_info_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,8 +11,44 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  List<Widget> _screenType = [
+    HomeScreen(),
+    FavoriteScreen(),
+    MyInfoScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Main Screen')));
+    return Scaffold(
+      body: _screenType.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.black,
+        selectedItemColor: Color(0xff14FF00),
+        unselectedItemColor: Colors.white,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
+        ],
+      ),
+    );
   }
 }
